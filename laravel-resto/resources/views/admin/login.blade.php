9ad70255f051a37df5c2413f14d23c08a072ef0a
cristiawandwip <subaza16@gmail.com>
2022-01-14 20:48:00 +0700

    Laravel-tailwind video 1-19
    tugas coding laravel

---
+++ laravel-resto/resources/views/admin/login.blade.php
@@ -0,0 +1,9 @@
+
+
+@extends('template.layout')
+
+@section('content')
+
+<h1 class="bg-red-300">Login Admin</h1>
+
+@endsection
---
+++ laravel-resto/tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: [
+    "./resources/views/*.blade.php",
+        "./resources/views/admin/*.blade.php",
+        "./resources/views/template/*.blade.php",
+  ],
+  theme: {
+    extend: {
+      
+      colors:{
+        "ungu":"#5267DF",
+        "merah":"#FA5959",
+        "biru":"#242A45",
+        "abu-abu":"#9194A2",
+        "putih":"#f7f7f7",
+      },
+
+        
+      container:{
+      center:true,
+      padding:"1rem",
+      screens:{
+        lg:"1124px",
+        xl:"1124px",
+        "2xl":"1124px",
+      },
+
+},
+
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
